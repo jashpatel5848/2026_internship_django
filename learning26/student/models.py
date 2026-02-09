939a8842e802d76fd8e2456f48bc630fe011a8d7
jashpatel5848 <jashpatel5848@gmail.com>
2026-02-09 22:39:36 +0530

    create model student & product

--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+#python class
+
+#parent class model
+#create table student(studentname varchar(100),studentage int,stuentcity varchar(40))
+#it will generate pk  automatically
+
+class Student(models.Model):
+    studentname = models.CharField(max_length=100)
+    studentage = models.IntegerField()
+    studentcity = models.CharField(max_length=40)
+    studentemail = models.EmailField(null=True)
+    
+    #meta class
+    class Meta:
+        db_table = "student" #table name
+
+class Product(models.Model):
+    productname = models.CharField(max_length=100)
+    productprice = models.IntegerField()
+    productdescription = models.TextField()
+    productstock = models.PositiveIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+
+
+    class Meta:
+        db_table = "product"
+
